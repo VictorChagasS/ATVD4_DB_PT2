f378160eb2271535c339b0782121a14fc6414aa3
Victor Chagas <42676345+VictorChagasS@users.noreply.github.com>
2024-04-10 11:31:27 -0300

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -25,4 +25,48 @@ db.connect((err) => {
   console.log("Conexão com o PostgreSQL estabelecida com sucesso.");
 });
 
+/*
+ const createTables = `
+    -- Tabela Usuario
+    CREATE TABLE IF NOT EXISTS wikiJogosLocal.Usuario (
+      ID SERIAL PRIMARY KEY,
+      Nome VARCHAR(80) NOT NULL,
+      Email VARCHAR(80) NOT NULL,
+      Senha VARCHAR(45) NOT NULL,
+      DataNascimento DATE
+    );
+
+    -- Tabela Comum
+    CREATE TABLE IF NOT EXISTS wikiJogosLocal.Comum (
+      Usuario_ID INT PRIMARY KEY,
+      FOREIGN KEY (Usuario_ID) REFERENCES wikiJogosLocal.Usuario(ID)
+    );
+
+    -- Tabela Jogo
+    CREATE TABLE IF NOT EXISTS wikiJogosLocal.Jogo (
+      ID SERIAL PRIMARY KEY,
+      Nome VARCHAR(45) NOT NULL,
+      Descricao VARCHAR(45) NOT NULL,
+      Desenvolvedor VARCHAR(45) NOT NULL,
+      Data DATE NOT NULL
+    );
+
+    -- Tabela Favoritos
+    CREATE TABLE IF NOT EXISTS wikiJogosLocal.Favoritos (
+      Comum_Usuario_ID INT NOT NULL,
+      Jogo_ID INT NOT NULL,
+      PRIMARY KEY (Comum_Usuario_ID, Jogo_ID),
+      FOREIGN KEY (Comum_Usuario_ID) REFERENCES wikiJogosLocal.Comum(Usuario_ID),
+      FOREIGN KEY (Jogo_ID) REFERENCES wikiJogosLocal.Jogo(ID)
+    );
+  `;
+*/
+
+  /*client.query(createTables, (err, result) => {
+    release();
+    if (err) {
+      return console.error("Erro ao criar tabelas", err.stack);
+    }
+    console.log("Tabelas criadas com sucesso.");
+  });*/
 export default db;
