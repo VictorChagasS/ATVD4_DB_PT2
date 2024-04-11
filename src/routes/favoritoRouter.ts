0534fa057bb11327c329112b0b65c91a2418fa5c
Victor Chagas <42676345+VictorChagasS@users.noreply.github.com>
2024-04-10 23:37:44 -0300

    Update favoritoRouter.ts

--- src/routes/favoritoRouter.ts
+++ src/routes/favoritoRouter.ts
@@ -2,10 +2,19 @@ import express, { Express } from "express";
 import favoritoController from "../controllers/favoritoController";
 const favoritoRouter: Express = express();
 
-favoritoRouter.get("/:id", favoritoController.listFavoritoById);
+favoritoRouter.get(
+  "/:comum_usuario_id/:jogo_id",
+  favoritoController.listFavoritoById
+);
 favoritoRouter.get("/", favoritoController.listAllFavoritos);
 favoritoRouter.post("/", favoritoController.createFavorito);
-favoritoRouter.delete("/:id", favoritoController.deleteFavorito);
-favoritoRouter.put("/:id", favoritoController.updateFavorito);
+favoritoRouter.delete(
+  "/:comum_usuario_id/:jogo_id",
+  favoritoController.deleteFavorito
+);
+favoritoRouter.put(
+  "/:comum_usuario_id/:jogo_id",
+  favoritoController.updateFavorito
+);
 
 export default favoritoRouter;
